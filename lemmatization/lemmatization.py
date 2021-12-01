38469bb6cdf0767d4ec3c40ba1142e244192341e
Łukasz Szarejko <lukaszszarejko@wp.pl>
2021-12-01 21:29:22 +0100

    improve lemmatization

--- lemmatization/lemmatization.py
+++ lemmatization/lemmatization.py
@@ -2,14 +2,17 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
 import csv
+import numpy as np
+import string
 
 
-def read_from_csv(path):
-    tweets = []
+def read_from_csv(path: string) -> np.array:
+    tweets = np.array([])
     with open(path, 'r', -1, 'utf8') as file:
         data = csv.reader(file)
         for row in data:
-            tweets.append(row[3])
+            tweets = np.append(tweets, row[3])
+        tweets = tweets[1:]
     return tweets
 
 
@@ -26,24 +29,30 @@ def tagger(nltk_tag):
         return None
 
 
-def lemmatization(text):
+def lemmatization(text: string) -> np.array:
     wnl = WordNetLemmatizer()
     tokens_tagged = nltk.pos_tag(nltk.word_tokenize(text))
     tokens_tagged = list(map(lambda x: (x[0], tagger(x[1])), tokens_tagged))
-    lemmatized_text = []
+    lemmatized_text = np.array([])
     for word, tag in tokens_tagged:
         if tag is None:
-            lemmatized_text.append((word, word))
+            lemmatized_text = np.append(lemmatized_text, word)
         else:
-            lemmatized_text.append((word, wnl.lemmatize(word, tag)))
+            lemmatized_text = np.append(lemmatized_text, wnl.lemmatize(word, tag))
     return lemmatized_text
+
+
+def lemmatize_all_csv(path: string) -> np.array:
+    tweets = read_from_csv(path)
+    words = np.array([])
+    for tweet in tweets:
+        lemmatized = lemmatization(tweet)
+        words = np.concatenate((words, lemmatized))
+    return words
 
 
 if __name__ == "__main__":
     text = "the bats saw sawing the cats with best stripes hanging upside down by their feet"
-    lemmatized_tokens = lemmatization(text)
-    print(lemmatized_tokens)
-    tweets = read_from_csv("data/train.csv")
-    for tweet in tweets:
-        print(lemmatization(tweet))
+    print(lemmatization(text))
+    print(lemmatize_all_csv("data/train.csv"))
         
