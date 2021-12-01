da3d56e5277c9686e494b47dd8739aa8ca08731f
matined <nedzimateusz@gmail.com>
2021-12-01 20:31:12 +0100

    typing added

--- stemming/utils.py
+++ stemming/utils.py
@@ -2,14 +2,15 @@ import numpy as np
 import nltk
 from nltk.stem import PorterStemmer
 import csv
+import typing
 
 
-def stem(word):
+def stem(word: str) -> str:
     porter = nltk.PorterStemmer()
     return porter.stem(word)
 
 
-def stem_array(array):
+def stem_array(array: np.array) -> np.array:
     words = np.array([])
     for text in array:
         words = np.append(words, text.split())
@@ -18,7 +19,7 @@ def stem_array(array):
     return words
 
 
-def stem_from_csv(csv_path, column):
+def stem_from_csv(csv_path: str, column: int) -> np.array:
     tweets = np.array([])
 
     with open(csv_path, 'r') as csv_file:
@@ -32,7 +33,7 @@ def stem_from_csv(csv_path, column):
     return words
 
 
-def save_to_csv(csv_path, words):
+def save_to_csv(csv_path: str, words: np.array) -> None:
     with open(csv_path, 'w') as new_file:
         csv_writer = csv.writer(new_file)
 
